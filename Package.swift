89dbd78d882135454ba80f5f56070e21253ab9fd
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2021-05-18 14:00:37 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
             name: "HDWalletKit",
             targets: ["HDWalletKit"]),
     ],
-    dependencies: [
-	.package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
-	.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.4.0"))
-    ],
+//    dependencies: [
+//	.package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
+//	.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.4.0"))
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
