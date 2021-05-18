b3c483ba56917bf9c0d05faea7a3d6fee102ec25
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2021-05-18 12:53:52 +0800

    update Package.swift

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HDWalletKit",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "HDWalletKit",
+            targets: ["HDWalletKit"]),
+    ],
+    platforms: [
+        .iOS(.v13)
+    ],
+    dependencies: [
+	.package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
+	.package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.4.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "HDWalletKit",
+            dependencies: []),
+        .testTarget(
+            name: "HDWalletKitTests",
+            dependencies: ["HDWalletKit"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
